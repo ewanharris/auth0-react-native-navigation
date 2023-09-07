0e694b7a0c2f498a42a2003fde5483db69820375
Ewan Harris <ewan.harris@okta.com>
2023-09-07 17:16:08 +0100

    Move to use hooks

---
+++ Home.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useAuth0} from 'react-native-auth0';
+
+export function Home() {
+  const {authorize, clearSession, user, isLoading} = useAuth0();
+
+  const onLogin = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const onLogout = async () => {
+    try {
+      await clearSession();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+
+  const loggedIn = user !== undefined && user !== null;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}> Auth0Sample - Login </Text>
+      <Text>You are{loggedIn ? ' ' : ' not '}logged in. </Text>
+      <Button
+        onPress={loggedIn ? onLogout : onLogin}
+        title={loggedIn ? 'Log Out' : 'Log In'}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
